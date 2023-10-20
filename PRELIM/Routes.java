6da3bcff86dd13a2f82082758bcfb13200ad58e5
Beirun <orillobernardjay@gmail.com>
2023-10-20 09:54:41 +0800

    commit

--- PRELIM/Routes.java
+++ PRELIM/Routes.java
@@ -3,28 +3,26 @@ public class Routes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String Barili, Dumanjug = "";
-        String Route = "\tCebu City\t\tSTART\n\tMingla\t\t\tRoute 1\n\tSan Fernando\tRoute 2\n\tCarcar\t\t\tRoute 3";
-
-        System.out.print("Input the speed: ");
+        String Route = "\tCebu City\t\tSTART\n\tMinglanilla\t\tRoute 1\n\tSan Fernando\tRoute 2\n\tCarcar\t\t\tRoute 3";
+        System.out.print("Input the speed (km/hr): ");
         double speed = sc.nextDouble();
         sc.nextLine();
         double distance;
-
-        System.out.print("Is Barili Obstructed? (Yes or No?)->");
+        System.out.print("Is Barili Obstructed? (Yes or No?)-> ");
         Barili = sc.nextLine().trim();
         if(Barili.equalsIgnoreCase("yes")) {
-            System.out.print("Is Dumanjug Obstructed? (Yes or No?)->");
+            System.out.print("Is Dumanjug Obstructed? (Yes or No?)-> ");
             Dumanjug = sc.nextLine().trim();
         }
         if(Barili.equalsIgnoreCase("yes") && Dumanjug.equalsIgnoreCase("yes")) {
             Route += "\n\tSibonga\t\t\tRoute 4\n\tArgao\t\t\tRoute 5\n\tRonda\t\t\tRoute 5.1\n\tAlcantara\t\tRoute 5.2";
-            distance = 110;
+            distance = 108;
         } else if(Dumanjug.equalsIgnoreCase("no")){
             Route += "\n\tSibonga\t\t\tRoute 4.2\n\tDumanjug\t\tRoute 4.2.1\n\tAlcantara\t\tRoute 4.2.2";
-            distance = 103;
+            distance = 98;
         } else {
             Route += "\n\tBarili\t\t\tRoute 4.1\n\tDumanjug\t\tRoute 4.1.1\n\tAlcantara\t\tRoute 4.1.2";
-            distance = 88.2;
+            distance = 84.9;
         }
         Route += "\n\tMoalboal\t\tEND";
         System.out.println("\nOutput: ");
